bc5668af22d6ca406e0bceae496587b26c5acce1
saurav <sauravpd58@gmail.com>
2018-07-31 00:26:34 +0530

    commit changes in TestLogic

--- src/test/java/codeSample/TestLogic.java
+++ src/test/java/codeSample/TestLogic.java
@@ -1,13 +1,28 @@
 package codeSample;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestLogic 
 {
+	
+	@Before
+	public void before()
+	{
+		System.out.println("****@Before****");
+	}
+	
+	@After
+	public void after()
+	{
+		System.out.println("****@After****");
+	}
 	@Test
 	public void testAddMethod()
 	{
+		System.out.println("testAddMethod");
 		Assert.assertEquals(10,Calculation.add(5, 5));
 	}
 }
