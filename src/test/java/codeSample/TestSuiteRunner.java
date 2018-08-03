6bd660642d4f519009cde0a9fea7632be50fe529
saurav <sauravpd58@gmail.com>
2018-08-03 23:43:56 +0530

    added javadoc

--- src/test/java/codeSample/TestSuiteRunner.java
+++ src/test/java/codeSample/TestSuiteRunner.java
@@ -2,7 +2,11 @@ package codeSample;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
+/**
+ * Test Suite Runner
+ * @author saurav
+ *
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestLogic.class,TimeoutTest.class})
 public class TestSuiteRunner 
